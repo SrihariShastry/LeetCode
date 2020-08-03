4d93b008524f7702575ed322e50039a774cf6812
SrihariShastry <sriharishastry.93@gmail.com>
2020-08-03 17:59:02 -0500

    buy and sell stock

---
+++ src/com/leetcode/top75/buySellStock.java
@@ -0,0 +1,20 @@
+package com.leetcode.top75;
+
+public class buySellStock {
+    public static void main(String[] args){
+        int[] prices = new int[]{7,1,5,3,6,4};
+        System.out.println("Max Profit: "+ maxProfit(prices));
+    }
+    private static int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE;
+        int maxProfit =0;
+        for(int price : prices){
+            if(price<minPrice)
+                minPrice=price;
+            else if(price-minPrice>maxProfit)
+                maxProfit=price-minPrice;
+        }
+        return maxProfit;
+    }
+}
+
