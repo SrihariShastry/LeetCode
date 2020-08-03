1090068b2f86b79b5b4ddb5f6529e27e5cb148f6
SrihariShastry <sriharishastry.93@gmail.com>
2020-08-03 17:53:08 -0500

    Two Sum

--- src/com/leetcode/top75/TwoSum.java
+++ src/com/leetcode/top75/TwoSum.java
@@ -5,10 +5,11 @@ import java.util.HashMap;
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{};
-        int target =9;
-        int[] res = new int[2];
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target =1;
+        int[] res;
         res = twoSum(nums,target);
+        System.out.println("[" +res[0]+ " , "+ res[1]+"]");
     }
     private static int[] twoSum(int[] nums,int target){
         HashMap<Integer,Integer> map = new HashMap<>();
