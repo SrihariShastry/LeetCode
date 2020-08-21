a6d5fe46ab76b68a2cb2f3d99dae5c72ccbe9c32
SrihariShastry <sriharishastry.93@gmail.com>
2020-08-21 12:47:57 -0500

    Partition Labels

---
+++ src/com/leetcode/top75/Arrays/PartitionLabels.java
@@ -0,0 +1,36 @@
+package com.leetcode.top75.Arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PartitionLabels {
+    class Solution {
+        public List<Integer> partitionLabels(String S) {
+            HashMap<Character,Integer> map = new HashMap<>();
+            int i=0;
+            for(char c: S.toCharArray()){
+                map.put(c,i);
+                i++;
+            }
+            int max=map.get(S.charAt(0));
+            int prevPartition=0;
+            List<Integer> res = new ArrayList<>();
+            for(i=0;i<S.length();i++){
+
+                if(map.get(S.charAt(i))>max||i>max)
+                    max=map.get(S.charAt(i));
+                if(i==max){
+                    res.add(i-prevPartition+1);
+                    prevPartition=i+1;
+                    continue;
+                }
+
+            }
+            if(prevPartition<i)
+                res.add(i-prevPartition);
+
+            return res;
+        }
+    }
+}
