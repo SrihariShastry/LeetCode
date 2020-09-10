e0a89220d6103df2fb2598be20c428ceeda41b32
SrihariShastry <sriharishastry.93@gmail.com>
2020-09-09 20:32:37 -0500

    Group Anagrams

---
+++ src/com/leetcode/top75/Arrays/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package com.leetcode.top75.Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            List<List<String>> res = new ArrayList<>();
+            HashMap<String, ArrayList<String>> anagrams= new HashMap<>();
+
+            if(strs.length==0)return res;
+
+            for(String s:strs){
+                // HashSet<String> letterSet = new HashSet<>(Arrays.asList(s.split("")));
+                char[] a=s.toCharArray();
+                Arrays.sort(a);
+                String key = String.valueOf(a);
+
+                ArrayList<String> word = anagrams.getOrDefault(key,new ArrayList<String>());
+                word.add(s);
+                anagrams.put(key,word);
+            }
+            for(ArrayList<String> str: anagrams.values()){
+                res.add(0,str);
+            }
+            return res;
+        }
+    }
+}
