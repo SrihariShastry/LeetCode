63b3ea265747e9ff62478c666bff0a3125498d1c
SrihariShastry <sriharishastry.93@gmail.com>
2020-11-10 18:16:32 -0700

    Symmetric Tree LC:101

---
+++ src/com/leetcode/top75/Arrays/SymmetricTree.java
@@ -0,0 +1,51 @@
+package com.leetcode.top75.Arrays;
+
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmetricTree {
+
+    /**
+     * Definition for a binary tree node.
+     **/
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+
+            q.offer(t1.left);
+            q.offer(t2.right);
+            q.offer(t1.right);
+            q.offer(t2.left);
+        }
+        return true;
+    }
+
+}
