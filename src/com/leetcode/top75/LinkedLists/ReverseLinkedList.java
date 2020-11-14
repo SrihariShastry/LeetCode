53f950fe68788b76025224a46e912cade21a7311
SrihariShastry <sriharishastry.93@gmail.com>
2020-11-13 22:14:33 -0700

    ReverseLinkedList LC: 206

---
+++ src/com/leetcode/top75/LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package com.leetcode.top75.LinkedLists;
+
+public class ReverseLinkedList {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+
+
+    }
+}
