78af7e6b67d288e7d6930753ca438b2be9fe4f5c
SrihariShastry <sriharishastry.93@gmail.com>
2020-11-25 12:39:37 -0700

    NonOverlappin Intervals: LC 435

---
+++ src/com/leetcode/top75/Matrix/NonOveralppingIntervals.java
@@ -0,0 +1,35 @@
+package com.leetcode.top75.Matrix;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class NonOveralppingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        //Sort according to Start time
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        LinkedList<int[]> res = new LinkedList<>();
+        int count = 0;
+        //Edge Case
+        if (intervals.length == 0) return 0;
+
+        res.addLast(intervals[0]);
+        int i = 1;
+        // for every overlapping interval, increase count
+        while (i < intervals.length) {
+            if (intervals[i][0] < res.peekLast()[1]) {
+                if (intervals[i][1] < res.peekLast()[1]) {
+                    res.removeLast();
+                    res.offerLast(intervals[i]);
+                }
+                count++;
+
+            } else {
+                res.offer(intervals[i]);
+            }
+            i++;
+        }
+        return count;
+    }
+}
