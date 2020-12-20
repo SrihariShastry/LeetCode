bf1e306b65671a406cc81350f7c6c5c5c19a59e0
SrihariShastry <sriharishastry.93@gmail.com>
2020-12-20 14:35:52 -0700

    Count Bits: LC :191

---
+++ src/com/leetcode/Companies/Qualcomm/CountBits.java
@@ -0,0 +1,20 @@
+package com.leetcode.Companies.Qualcomm;
+
+public class CountBits {
+    /**
+     * Idea behind the solution is that when we "&" n and n-1, the last significant bit of the number flips.
+     * this process continues until n becomes 0;
+     *
+     * @param n Number
+     * @return the number of 1 bits in the number
+     */
+
+    public int hammingWeight(int n) {
+        int bits = 0;
+        while (n != 0) {
+            bits++;
+            n = n & (n - 1);
+        }
+        return bits;
+    }
+}
